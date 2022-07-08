abaa4cdc919e8b8a4fcf01fbade267456ac5e478
Linxae <702611663@qq.com>
2022-07-09 01:11:09 +0800

    day3 主页文章栏样式

--- fe-vue3/src/views/home/downMain.vue
+++ fe-vue3/src/views/home/downMain.vue
@@ -139,25 +139,91 @@
             <div class="art-side-bar w">
                 <ul class="articles-area">
                     <li class="article-container">
+                        <div class="article-img"><div class="wrap"></div></div>
                         <div class="article-info">
-                            <div class="time">
-                                <el-icon :size="size" :color="color">
-                                    <Edit /> </el-icon
-                                >发布于
-                                <span>2022-01-03</span>
+                            <div class="header">
+                                <span class="title">2021年度总结</span>
+                                <ul class="tags">
+                                    <span
+                                        ><img
+                                            src="../../assets/iconfont/tags.png"
+                                            alt=""
+                                            style="width: 25px; height: 25px"
+                                    /></span>
+                                    <!-- <el-tag
+                                        v-for="item in items"
+                                        :key="item.label"
+                                        :type="item.type"
+                                        class="mx-1"
+                                        effect="dark"
+                                        round
+                                    >
+                                        {{ item.label }}
+                                    </el-tag> -->
+                                    <el-tag class="tag-item" round>
+                                        前端 </el-tag
+                                    ><el-tag class="tag-item" round>
+                                        前端 </el-tag
+                                    ><el-tag class="tag-item" round>
+                                        前端 </el-tag
+                                    ><el-tag class="tag-item" round>
+                                        前端 </el-tag
+                                    ><el-tag class="tag-item" round>
+                                        前端
+                                    </el-tag>
+                                </ul>
                             </div>
-                            <div class="title">
-                                <span>2021年度总结</span>
-                            </div>
-                            <div class="extend-info">
-                                <div class="flow"><span>1550热度</span></div>
-                                <h4 class="comment"><span>2条评论</span></h4>
-                            </div>
-                            <span class="article-part"
-                                >iconfont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具</span
+                            <span class="article-part">
+                                iconfont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具iconfont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具iconfont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具iconfont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具iconfont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具</span
                             >
+                            <div class="footer">
+                                <div class="time">
+                                    <img
+                                        src="../../assets/iconfont/时间戳.png"
+                                        style="width: 25px; height: 25px"
+                                        alt=""
+                                    />
+                                    <span style="margin-left: 5px"
+                                        >发布于 2022-01-03</span
+                                    >
+                                </div>
+                                <div class="fixed-top">
+                                    <el-tag
+                                        effect="dark"
+                                        type="danger"
+                                        class="tag-top"
+                                        round
+                                        size="large"
+                                        v-show="true"
+                                    >
+                                        置顶!
+                                    </el-tag>
+                                </div>
+                                <div class="extend-info">
+                                    <div class="comment">
+                                        <img
+                                            src="../../assets/iconfont/消息.png"
+                                            style="width: 25px; height: 25px"
+                                            alt=""
+                                        /><span style="margin-left: 5px"
+                                            >2 条评论</span
+                                        >
+                                    </div>
+                                    <div class="go-detail">
+                                        <el-tag
+                                            effect="dark"
+                                            type="success"
+                                            class="tag-top"
+                                            round
+                                            size="large"
+                                            v-show="true"
+                                        >
+                                            阅读详细
+                                        </el-tag>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="article-img"></div>
                     </li>
                     <li></li>
                     <li></li>
